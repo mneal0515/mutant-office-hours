d73a004ee0b478b679ad14e4210dd87d853e43d1
Matt <mattneal@umail.iu.edu>
2016-06-28 11:46:24 -0400

    update css dependencies, update errors to no longer console log

---
+++ app/mutantList/directives/directives.mutantTable.js
@@ -0,0 +1,40 @@
+(function () {
+    'use strict';
+    
+    angular.module('mutantApp.mutantList').directive('xtMutantTable', xtMutantTable);
+    
+    function xtMutantTable() {
+        return {
+            templateUrl: 'app/mutantList/directives/mutantTable.html',
+            restrict: 'E',
+            controller: MutantTableController,
+            controllerAs: 'vm',
+            bindToController: true,
+            scope: {
+                mutants: '='
+            }
+        };
+    }
+
+    MutantTableController.$inject = ['textMessageService'];
+
+    function MutantTableController(textMessageService) {
+        var vm = this;
+        
+        vm.toggleComplete = toggleComplete;
+        vm.deleteMutant = deleteMutant;
+        vm.sendText = sendText;
+
+        function toggleComplete(mutant) {
+            vm.mutants.$save(mutant);
+        }
+
+        function deleteMutant(mutant) {
+            vm.mutants.$remove(mutant);
+        }
+
+        function sendText(mutant) {
+            textMessageService.sendText(mutant, vm.mutants);
+        }
+    }
+})();
